7ee7b411cb9b281bf2836c1988e9c1b097974350
monirulislam9549 <monirulislam9549@gmail.com>
2023-05-20 16:10:26 +0600

    gallery added

---
+++ src/components/Gallery/Gallery.jsx
@@ -0,0 +1,93 @@
+const Gallery = () => {
+  return (
+    <div className="my-6">
+      <h1 className="text-center font-bold text-5xl my-10 text-green-500">
+        Toy Gallery
+      </h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
+        <div className="card card-compact w-96 bg-base-100 shadow-xl">
+          <figure>
+            <img src={"https://i.ibb.co/pd9bHwm/images-1.jpg"} alt="Shoes" />
+          </figure>
+        </div>
+        <div className="card card-compact w-96 bg-base-100 shadow-xl">
+          <figure>
+            <img src={"https://i.ibb.co/WvMNMJ6/images-3.jpg"} alt="" />
+          </figure>
+        </div>
+        <div className="card card-compact w-96 bg-base-100 shadow-xl">
+          <figure>
+            <img src={"https://i.ibb.co/gtLVBTh/images-4.jpg"} alt="" />
+          </figure>
+        </div>
+        <div className="card card-compact w-96 bg-base-100 shadow-xl">
+          <figure>
+            <img src={"https://i.ibb.co/Hxxzs9Q/images-2.jpg"} alt="" />
+          </figure>
+        </div>
+        <div className="card card-compact w-96 bg-base-100 shadow-xl">
+          <figure>
+            <img src={"https://i.ibb.co/Q9vqK0N/images-5.jpg"} alt="" />
+          </figure>
+        </div>
+        <div className="card card-compact w-96 bg-base-100 shadow-xl">
+          <figure>
+            <img src={"https://i.ibb.co/P6ckWMY/images-6.jpg"} alt="" />
+          </figure>
+        </div>
+        <div className="card card-compact w-96 bg-base-100 shadow-xl">
+          <figure>
+            <img src={"https://i.ibb.co/tpnZxrz/images-7.jpg"} alt="" />
+          </figure>
+        </div>
+        <div className="card card-compact w-96 bg-base-100 shadow-xl">
+          <figure>
+            <img src={"https://i.ibb.co/pd9bHwm/images-1.jpg"} alt="Shoes" />
+          </figure>
+        </div>
+        <div className="card card-compact w-96 bg-base-100 shadow-xl">
+          <figure>
+            <img src={"https://i.ibb.co/pd9bHwm/images-1.jpg"} alt="Shoes" />
+          </figure>
+        </div>
+        <div className="card card-compact w-96 bg-base-100 shadow-xl">
+          <figure>
+            <img src={"https://i.ibb.co/P6ckWMY/images-6.jpg"} alt="" />
+          </figure>
+        </div>
+        <div className="card card-compact w-96 bg-base-100 shadow-xl">
+          <figure>
+            <img src={"https://i.ibb.co/qxk9K83/images-8.jpg"} alt="" />
+          </figure>
+        </div>
+        <div className="card card-compact w-96 bg-base-100 shadow-xl">
+          <figure>
+            <img src={"https://i.ibb.co/wy1bcRz/images-10.jpg"} alt="" />
+          </figure>
+        </div>
+        <div className="card card-compact w-96 bg-base-100 shadow-xl">
+          <figure>
+            <img src={"https://i.ibb.co/s50LTdr/images-12.jpg"} alt="" />
+          </figure>
+        </div>
+        <div className="card card-compact w-96 bg-base-100 shadow-xl">
+          <figure>
+            <img src={"https://i.ibb.co/b6QHyhz/images-13.jpg"} alt="" />
+          </figure>
+        </div>
+        <div className="card card-compact w-96 bg-base-100 shadow-xl">
+          <figure>
+            <img src={"https://i.ibb.co/gT95DvS/14.jpg"} alt="" />
+          </figure>
+        </div>
+        <div className="card card-compact w-96 bg-base-100 shadow-xl">
+          <figure>
+            <img src={"https://i.ibb.co/xCH7440/images-15.jpg"} alt="" />
+          </figure>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Gallery;
